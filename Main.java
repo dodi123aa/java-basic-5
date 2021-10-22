138c6d0780be8fd44c216c0cfcfffa28885ea2f9
unknown <dodykurniawan477@gmail.com>
2021-10-22 21:10:50 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,66 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero("Dody Kurniawan", 21);
+
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+
+        hero.run();
+
+        hero.eat();
+        hero.eat();
+        hero.eat();
+        hero.eat();
+
+        hero.drink();
+        hero.drink();
+
+        hero.jump();
+        hero.jump();
+        hero.jump();
+        hero.jump();
+        hero.jump();
+        hero.jump();
+
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+
+        hero.sit();
+        hero.sit();
+        hero.sit();
+        hero.sit();
+        hero.sit();
+
+        hero.attack();
+
+        hero.player();
+
+
+
+}
+}
